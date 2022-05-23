6fdfa9a60bf1426358241f12f32543a03d523c1a
Donglin Y <513399457@qq.com>
2022-05-23 16:59:03 -0700

    add resnet

--- resnet.py
+++ resnet.py
@@ -96,6 +96,7 @@ def eval_step(model, x, y, metrics):
 
 def pack_dtensor_inputs(images, labels, image_layout, label_layout):
   num_local_devices = image_layout.mesh.num_local_devices()
+  print(images.shape)
   images = tf.split(images, num_local_devices)
   labels = tf.split(labels, num_local_devices)
   print(images.shape)
