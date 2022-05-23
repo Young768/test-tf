1cb211b8b72e46469240632008a0084948119217
Donglin Y <513399457@qq.com>
2022-05-23 16:48:50 -0700

    add resnet

--- resnet.py
+++ resnet.py
@@ -48,7 +48,7 @@ batch_size = 128
 ds_train = ds_train.map(
     normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
 ds_train = ds_train.cache()
-ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+ds_train = ds_train.shuffle(1000)
 ds_train = ds_train.batch(batch_size)
 ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
 
