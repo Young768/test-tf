32711da8750efcb8c27d96561eaaa904bee8eeab
Donglin Y <513399457@qq.com>
2022-05-25 00:45:39 -0700

    small bs.

--- resnet-50-benchmark.py
+++ resnet-50-benchmark.py
@@ -731,7 +731,7 @@ def eval_step(model, x, y):
   logits = model(x, training=False)
   loss = tf.reduce_sum(tf.keras.losses.sparse_categorical_crossentropy(
       y, logits, from_logits=False))
-  val_loss.update_state(loss)
+  #val_loss.update_state(loss)
   val_top1.update_state(y, logits)
   val_top5.update_state(y, logits)
   return  loss
