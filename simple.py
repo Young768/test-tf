e32f676c0f1f843f0c4fbb399d5398275d9ff621
Donglin Y <513399457@qq.com>
2022-08-30 22:05:31 -0700

    add test.

--- simple.py
+++ simple.py
@@ -1,21 +1,16 @@
 import tensorflow as tf
 
-@tf.function
-def step(tensor):
-    out = tf.add(tensor, 1.0)
-    out = tf.multiply(out, 2.0)
-    return out
+inputs = tf.keras.Input((28, 28, 1))
+x = tf.keras.layers.BatchNormalization()(inputs)
+x = tf.keras.layers.BatchNormalization()(x)
+output = tf.keras.layers.BatchNormalization()(x)
+model = tf.keras.Model(inputs, output)
 
 @tf.function
-def step_1(tensor):
-    with tf.GradientTape() as tape:
-        out = step(tensor) * 10
-    grads = tape.gradient(out, tensor)
-    tensor.assign_add(grads)
-    return out, grads
+def foo(inputs):
+  return model(inputs)
 
-input_tensor = tf.Variable(initial_value=1.0)
-for i in range(4):
-    # input_tensor = tf.constant(i, dtype=tf.float32)
-    output_tensor = step_1(input_tensor)
-    print("{}".format(i), output_tensor)
+inp = tf.constant(value=0, shape=(1, 28, 28, 1))
+for i in range(40):
+    out = foo(inp)
+print("Done.")
