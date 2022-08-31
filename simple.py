69fc8f4d978a204d931540ed44497ff91a26c684
Donglin Y <513399457@qq.com>
2022-08-30 23:41:38 -0700

    make it training.

--- simple.py
+++ simple.py
@@ -6,13 +6,14 @@ x = tf.keras.layers.BatchNormalization()(x)
 output = tf.keras.layers.BatchNormalization()(x)
 model = tf.keras.Model(inputs, output)
 
-def step_1(tensor):
+@tf.function
+def step_1(model, tensor):
     with tf.GradientTape() as tape:
-        out = model(tensor)
+        out = model(tensor, training=True)
     gradients = tape.gradient(out, tensor)
     return gradients
 
 inp = tf.constant(value=1.0, shape=(1, 28, 28, 1))
 for i in range(40):
-    output_tensor = step_1(inp)
+    output_tensor = step_1(model, inp)
 print("Done.")
