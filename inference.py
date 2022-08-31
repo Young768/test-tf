ea65703effc80d7865be69b24f8d8e99edd80250
Donglin Y <513399457@qq.com>
2022-08-31 15:03:53 -0700

    add inference.

---
+++ inference.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+inputs = tf.keras.Input((28, 28, 1))
+x = tf.keras.layers.BatchNormalization()(inputs)
+x = tf.keras.layers.BatchNormalization()(x)
+output = tf.keras.layers.BatchNormalization()(x)
+model = tf.keras.Model(inputs, output)
+
+@tf.function
+def foo(inputs):
+  return model(inputs)
+
+inp = tf.constant(value=1.0, shape=(1, 28, 28, 1))
+for i in range(40):
+    output_tensor = foo(inp)
+print("Done.")
