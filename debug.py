1d60f29ae0fde6b1f623441cd9f72d26fb524efa
Donglin Y <513399457@qq.com>
2022-09-09 11:18:42 -0700

    add debug.

--- debug.py
+++ debug.py
@@ -2,17 +2,20 @@ import tensorflow as tf
 import os
 os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"
 
+
+inputs = tf.keras.Input((28, 28, 1))
+x = tf.keras.layers.MaxPooling2D((2, 2), name='pool1')(inputs)
+x = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', name='conv2')(x)
+output = tf.keras.layers.MaxPooling2D((2, 2), name='pool2')(x)
+model = tf.keras.Model(inputs, output)
 @tf.function
 def step(tensor):
-    inputs = tf.keras.Input((10, 1))(tensor)
-    x = tf.keras.layers.MaxPooling2D((2, 2), name='pool1')(inputs)
-    x = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', name='conv2')(x)
-    output = tf.keras.layers.MaxPooling2D((2, 2), name='pool2')(x)
+    output = model(tensor)
     return output
 
 
 for i in range(40):
-    inp = tf.constant(value=1.0, shape=(1, 10, 1))
+    inp = tf.constant(value=1.0, shape=(1, 28, 28, 1))
     output_tensor = step(inp)
-    print("{}".format(i), output_tensor)
+    print("output: {}".format(i), output_tensor)
     # input_tensor.assign_add(output_tensor[1])
