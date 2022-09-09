8091b1c2039eae78b2d440bb3e96267668119dc0
Donglin Y <513399457@qq.com>
2022-09-09 11:35:19 -0700

    add debug.

--- simple.py
+++ simple.py
@@ -1,19 +1,80 @@
 import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import os
+import time
+os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"
 
-inputs = tf.keras.Input((28, 28, 1))
-#x = tf.keras.layers.BatchNormalization()(inputs)
-#x = tf.keras.layers.BatchNormalization()(x)
-output = tf.keras.layers.BatchNormalization()(inputs)
-model = tf.keras.Model(inputs, output)
+(train_images, train_labels), (test_images, test_labels) = keras.datasets.cifar10.load_data()
+
+CLASS_NAMES= ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+validation_images, validation_labels = train_images[:5000], train_labels[:5000]
+train_images, train_labels = train_images[5000:], train_labels[5000:]
+
+train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
+validation_ds = tf.data.Dataset.from_tensor_slices((validation_images, validation_labels))
+
+def process_images(image, label):
+    # Normalize images to have a mean of 0 and standard deviation of 1
+    image = tf.image.per_image_standardization(image)
+    # Resize images from 32x32 to 277x277
+    image = tf.image.resize(image, (227,227))
+    return image, label
+
+train_ds_size = tf.data.experimental.cardinality(train_ds).numpy()
+test_ds_size = tf.data.experimental.cardinality(test_ds).numpy()
+validation_ds_size = tf.data.experimental.cardinality(validation_ds).numpy()
+print("Training data size:", train_ds_size)
+print("Test data size:", test_ds_size)
+print("Validation data size:", validation_ds_size)
+
+train_ds = (train_ds
+                  .map(process_images)
+                  .shuffle(buffer_size=train_ds_size)
+                  .batch(batch_size=32, drop_remainder=True))
+
+test_ds = (test_ds
+                  .map(process_images)
+                  .shuffle(buffer_size=train_ds_size)
+                  .batch(batch_size=32, drop_remainder=True))
+
+validation_ds = (validation_ds
+                  .map(process_images)
+                  .shuffle(buffer_size=train_ds_size)
+                  .batch(batch_size=32, drop_remainder=True))
+
+model = keras.models.Sequential([
+    keras.layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape=(227,227,3)),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+    keras.layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+    keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+    keras.layers.BatchNormalization(),
+    keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+    keras.layers.BatchNormalization(),
+    keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+    keras.layers.Flatten(),
+    keras.layers.Dense(4096, activation='relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(4096, activation='relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(10, activation='softmax')
+])
 
 @tf.function
-def step_1(model, tensor):
-    with tf.GradientTape() as tape:
-        out = model(tensor, training=True)
-    gradients = tape.gradient(out, tensor)
-    return gradients
+def step(tensor):
+    output = model(tensor)
+    return output
 
-inp = tf.constant(value=1.0, shape=(1, 28, 28, 1))
+input = iter(train_ds)
 for i in range(40):
-    output_tensor = step_1(model, inp)
-print("Done.")
+    for x, y in next(input):
+        output_tensor = step(x)
+        print("output: {}".format(i), output_tensor)
+    # input_tensor.assign_add(output_tensor[1])
