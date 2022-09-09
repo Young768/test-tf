1f256740befed5c0206b69a771426ea00c56704f
Donglin Y <513399457@qq.com>
2022-09-09 12:44:43 -0700

    fix.

--- simple.py
+++ simple.py
@@ -7,25 +7,7 @@ os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"
 
 model = keras.models.Sequential([
     keras.layers.Conv2D(64, (3, 3), activation='relu', name='conv2'),
-    #keras.layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape=(227,227,3)),
-    keras.layers.BatchNormalization(),
-    #keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
-    #keras.layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
-    #keras.layers.BatchNormalization(),
-    #keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
-    #keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-    #keras.layers.BatchNormalization(),
-    #keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-    #keras.layers.BatchNormalization(),
-    #keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-    #keras.layers.BatchNormalization(),
-    #keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
-    #keras.layers.Flatten(),
-    #keras.layers.Dense(4096, activation='relu'),
-    #keras.layers.Dropout(0.5),
-    #keras.layers.Dense(4096, activation='relu'),
-    #keras.layers.Dropout(0.5),
-    #keras.layers.Dense(10, activation='softmax')
+    keras.layers.BatchNormalization()
 ])
 
 @tf.function
