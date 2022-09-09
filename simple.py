a489a7d74964cc00831738400c1dfd7f4cc09582
Donglin Y <513399457@qq.com>
2022-09-09 12:41:16 -0700

    fix.

--- simple.py
+++ simple.py
@@ -5,46 +5,6 @@ import os
 import time
 os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"
 
-(train_images, train_labels), (test_images, test_labels) = keras.datasets.cifar10.load_data()
-
-CLASS_NAMES= ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-
-validation_images, validation_labels = train_images[:5000], train_labels[:5000]
-train_images, train_labels = train_images[5000:], train_labels[5000:]
-
-train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
-test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
-validation_ds = tf.data.Dataset.from_tensor_slices((validation_images, validation_labels))
-
-def process_images(image, label):
-    # Normalize images to have a mean of 0 and standard deviation of 1
-    image = tf.image.per_image_standardization(image)
-    # Resize images from 32x32 to 277x277
-    image = tf.image.resize(image, (227,227))
-    return image, label
-
-train_ds_size = tf.data.experimental.cardinality(train_ds).numpy()
-test_ds_size = tf.data.experimental.cardinality(test_ds).numpy()
-validation_ds_size = tf.data.experimental.cardinality(validation_ds).numpy()
-print("Training data size:", train_ds_size)
-print("Test data size:", test_ds_size)
-print("Validation data size:", validation_ds_size)
-
-train_ds = (train_ds
-                  .map(process_images)
-                  .shuffle(buffer_size=train_ds_size)
-                  .batch(batch_size=32, drop_remainder=True))
-
-test_ds = (test_ds
-                  .map(process_images)
-                  .shuffle(buffer_size=train_ds_size)
-                  .batch(batch_size=32, drop_remainder=True))
-
-validation_ds = (validation_ds
-                  .map(process_images)
-                  .shuffle(buffer_size=train_ds_size)
-                  .batch(batch_size=32, drop_remainder=True))
-
 model = keras.models.Sequential([
     keras.layers.Conv2D(64, (3, 3), activation='relu', name='conv2'),
     #keras.layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape=(227,227,3)),
