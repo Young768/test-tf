c96a123391a5adff7b92a2d4456a52de8bbad60d
Donglin Y <513399457@qq.com>
2022-09-09 10:24:05 -0700

    add debug.

---
+++ debug.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+import os
+os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"
+
+@tf.function
+def step(tensor):
+    out = tf.add(tensor, 1.0)
+    out = tf.add(out, 1.0)
+    out = tf.add(out, 1.0)
+    out = tf.add(out, 1.0)
+    out = tf.add(out, 1.0)
+    out = tf.add(out, 1.0)
+    return out
+
+# input_tensor = tf.Variable(initial_value=1.0)
+for i in range(20):
+    input_tensor = tf.Variable(initial_value=1.0)
+    output_tensor = step(input_tensor)
+    print("{}".format(i), output_tensor)
+    # input_tensor.assign_add(output_tensor[1])
