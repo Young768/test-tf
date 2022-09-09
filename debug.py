1604a3e16486c7ad45eaa471aef636ccb1ccadf1
Donglin Y <513399457@qq.com>
2022-09-09 11:15:42 -0700

    add debug.

--- debug.py
+++ debug.py
@@ -4,17 +4,15 @@ os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"
 
 @tf.function
 def step(tensor):
-    out = tf.multiply(tensor, 2.0)
-    out = tf.multiply(out, 2.0)
-    out = tf.multiply(out, 2.0)
-    out = tf.multiply(out, 2.0)
-    out = tf.multiply(out, 2.0)
-    out = tf.multiply(out, 2.0)
-    return out
+    inputs = tf.keras.Input((10, 1))(tensor)
+    x = tf.keras.layers.MaxPooling2D((2, 2), name='pool1')(inputs)
+    x = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', name='conv2')(x)
+    output = tf.keras.layers.MaxPooling2D((2, 2), name='pool2')(x)
+    return output
 
-# input_tensor = tf.Variable(initial_value=1.0)
+
 for i in range(40):
-    input_tensor = tf.Variable(initial_value=1.0)
-    output_tensor = step(input_tensor)
+    inp = tf.constant(value=1.0, shape=(1, 10, 1))
+    output_tensor = step(inp)
     print("{}".format(i), output_tensor)
     # input_tensor.assign_add(output_tensor[1])
