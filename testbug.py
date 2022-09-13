d9a114a395dfbe3b838476233543d2d1ce70eeed
Donglin Y <513399457@qq.com>
2022-09-13 12:37:49 -0700

    fix.

--- testbug.py
+++ testbug.py
@@ -32,7 +32,7 @@ for i in range(40):
     inp = tf.constant(value=1.0, shape=(1, 28, 28, 1))
     dataset = tf.data.Dataset.from_tensor_slices(inp).repeat().batch(1)
     #output_tensor = step(dataset)
-    res = model.predict(inp)
+    output_tensor = model.predict(inp)
     if i == 0:
         prev = output_tensor
     if i > 0:
