7a2c95f1182a6d082eab23f9de82ac42e7f3efd5
Donglin Y <513399457@qq.com>
2022-09-14 14:28:19 -0700

    log predict only.

--- testbug.py
+++ testbug.py
@@ -37,7 +37,7 @@ def step(tensor):
 for i in range(1):
     inp = tf.constant(value=1.0, shape=(1, 28, 28, 1))
     dataset = tf.data.Dataset.from_tensor_slices(inp).repeat().batch(1)
-    output_tensor = model.predict(dataset, steps=40, callbacks=[CustomCallback(inp)])
+    output_tensor = model.predict(dataset, steps=40)
     if i == 0:
         prev = output_tensor
     if i > 0:
