95cbbea4233e1cffd6a4ed3529e8f8a1c87296e6
Donglin Y <513399457@qq.com>
2022-10-20 14:39:56 -0700

    update alexnet

--- testbug.py
+++ testbug.py
@@ -3,47 +3,57 @@ from tensorflow import keras
 import matplotlib.pyplot as plt
 import os
 import time
-from keras.callbacks import Callback
 
-os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"
+(train_images, train_labels), (test_images, test_labels) = keras.datasets.cifar10.load_data()
+
+CLASS_NAMES= ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+validation_images, validation_labels = train_images[:5000], train_labels[:5000]
+train_images, train_labels = train_images[5000:], train_labels[5000:]
+
+train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
+validation_ds = tf.data.Dataset.from_tensor_slices((validation_images, validation_labels))
+
+def process_images(image, label):
+    # Normalize images to have a mean of 0 and standard deviation of 1
+    image = tf.image.per_image_standardization(image)
+    # Resize images from 32x32 to 277x277
+    image = tf.image.resize(image, (227,227))
+    return image, label
+
+train_ds_size = tf.data.experimental.cardinality(train_ds).numpy()
+test_ds_size = tf.data.experimental.cardinality(test_ds).numpy()
+validation_ds_size = tf.data.experimental.cardinality(validation_ds).numpy()
+print("Training data size:", train_ds_size)
+print("Test data size:", test_ds_size)
+print("Validation data size:", validation_ds_size)
+
+train_ds = (train_ds
+                  .map(process_images)
+                  .shuffle(buffer_size=train_ds_size)
+                  .batch(batch_size=32, drop_remainder=True))
+
+test_ds = (test_ds
+                  .map(process_images)
+                  .shuffle(buffer_size=train_ds_size)
+                  .batch(batch_size=32, drop_remainder=True))
+
+validation_ds = (validation_ds
+                  .map(process_images)
+                  .shuffle(buffer_size=train_ds_size)
+                  .batch(batch_size=32, drop_remainder=True))
 
 model = keras.models.Sequential([
-    keras.layers.MaxPooling2D((2, 2), name='pool1'),
-    keras.layers.Conv2D(64, (3, 3), activation='relu', name='conv2'),
-    keras.layers.MaxPooling2D((2, 2), name='pool2'),
-    keras.layers.BatchNormalization()
+    keras.layers.BatchNormalization(),
+    #keras.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+    keras.layers.Flatten(),
 ])
 
-optimizer = tf.keras.optimizers.SGD()
-model.compile(optimizer)
-test_layer = model.layers[3]
-tf.print("debugggggggggggggggggg",test_layer)
-class CustomCallback(tf.keras.callbacks.Callback):
-   def __init__(self, data):
-        self.data = data
-   def on_predict_batch_end(self, epoch, logs=None):
-        out = model.layers[0](self.data)
-        out = model.layers[1](out)
-        out = model.layers[2](out)
-        encoder_outputs = test_layer(out)
-        tf.print('output --> ', encoder_outputs)
+model.compile(loss='sparse_categorical_crossentropy', optimizer=tf.optimizers.SGD(lr=0.001), metrics=['accuracy'])
 
-@tf.function
-def step(tensor):
-    output = model(tensor)
-    return output
+model.fit(train_ds,
+          epochs=50,
+          steps_per_epoch=20)
 
 
-for i in range(40):
-    inp = tf.constant(value=1.0, shape=(1, 28, 28, 1))
-    #dataset = tf.data.Dataset.from_tensor_slices(inp).repeat().batch(1)
-    #output_tensor = model.predict(dataset, steps=40)
-    output_tensor = step(inp)
-    if i == 0:
-        prev = output_tensor
-    if i > 0:
-        tf.debugging.assert_near(output_tensor, prev)
-        prev = output_tensor
-
-    #print("output: {}".format(i), output_tensor)
-    # input_tensor.assign_add(output_tensor[1])
