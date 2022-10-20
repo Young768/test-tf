bc014de9b6c209aeeb720d70ae2e10e9e1fb567c
Donglin Y <513399457@qq.com>
2022-10-20 13:54:43 -0700

    update alexnet

--- simple.py
+++ simple.py
@@ -5,9 +5,62 @@ import os
 import time
 os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"
 
-model = keras.models.Sequential([
-    keras.layers.Conv2D(64, (3, 3), activation='relu', name='conv2'),
-    keras.layers.BatchNormalization()
+model = tf.keras.Sequential([
+    # layer 1
+    tf.keras.layers.Conv2D(filters=96,
+                            kernel_size=(11, 11),
+                            strides=4,
+                            padding="valid",
+                            activation=tf.keras.activations.relu,
+                            input_shape=(227, 227, 3)),
+    tf.keras.layers.MaxPool2D(pool_size=(3, 3),
+                                strides=2,
+                                padding="valid"),
+    tf.keras.layers.BatchNormalization(),
+    # layer 2
+    tf.keras.layers.Conv2D(filters=256,
+                            kernel_size=(5, 5),
+                            strides=1,
+                            padding="same",
+                            activation=tf.keras.activations.relu),
+    tf.keras.layers.MaxPool2D(pool_size=(3, 3),
+                                strides=2,
+                                padding="same"),
+    tf.keras.layers.BatchNormalization(),
+    # layer 3
+    tf.keras.layers.Conv2D(filters=384,
+                            kernel_size=(3, 3),
+                            strides=1,
+                            padding="same",
+                            activation=tf.keras.activations.relu),
+    # layer 4
+    tf.keras.layers.Conv2D(filters=384,
+                            kernel_size=(3, 3),
+                            strides=1,
+                            padding="same",
+                            activation=tf.keras.activations.relu),
+    # layer 5
+    tf.keras.layers.Conv2D(filters=256,
+                            kernel_size=(3, 3),
+                            strides=1,
+                            padding="same",
+                            activation=tf.keras.activations.relu),
+    tf.keras.layers.MaxPool2D(pool_size=(3, 3),
+                                strides=2,
+                                padding="same"),
+    tf.keras.layers.BatchNormalization(),
+    # layer 6
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(units=4096,
+                            activation=tf.keras.activations.relu),
+    tf.keras.layers.Dropout(rate=0.2),
+    # layer 7
+    tf.keras.layers.Dense(units=4096,
+                            activation=tf.keras.activations.relu),
+    tf.keras.layers.Dropout(rate=0.2),
+    # layer 8
+    tf.keras.layers.Dense(units=10,
+                            activation=tf.keras.activations.softmax)
 ])
 
 @tf.function
